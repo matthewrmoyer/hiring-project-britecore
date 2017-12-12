0bdbe4ef4a5fb4e65f0493fbe75a157f137a79a4
Unknown <matthewrmoyer@gmail.com>
2017-12-12 10:44:34 -0700

    pre-refactor-commit

--- src/store/modules/addRisk/mutations.js
+++ src/store/modules/addRisk/mutations.js
@@ -9,13 +9,6 @@ export const addField = (state, payload) => {
 
 export const addFieldValue = (state, payload) => {
     let targetField = state.fields.filter(field => field.name==payload.fieldName)
-    console.log(targetField[0]);   
-    console.log(state.fields.indexOf(targetField[0]));
     let targetIndex = state.fields.indexOf(targetField[0])
-    state.fields[targetIndex].value = payload.fieldValue
-    
+    state.fields[targetIndex].value = payload.fieldValue   
 }
-    
-    
-    
-    // state.fields[payload.fieldName]['value'] = payload.fieldValue
